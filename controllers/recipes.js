62081199cbd1f464c0f5d7c8513b0c1d4e891228
Chris Legassick <chrislegassick@Chriss-Air.broadband>
2020-05-25 23:08:19 +0100

    Created asyncHandler to apply DRY to controller

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,103 +1,84 @@
 const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
 const Recipe = require('../models/Recipe');
 
 // @desc    Get all recipes
 // @route   GET /api/v1/recipes
 // @access  Public
-exports.getRecipes = async (req, res, next) => {
-  try {
-    const recipes = await Recipe.find();
+exports.getRecipes = asyncHandler(async (req, res, next) => {
+  const recipes = await Recipe.find();
 
-    res.status(200).json({
-      success: true,
-      count: recipes.length,
-      data: recipes
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(200).json({
+    success: true,
+    count: recipes.length,
+    data: recipes
+  });
+});
 
 // @desc    Get single recipe by id
 // @route   GET /api/v1/recipes/:id
 // @access  Public
-exports.getRecipe = async (req, res, next) => {
-  try {
-    const recipe = await Recipe.findById(req.params.id);
-    if (!recipe) {
-      return next(
-        new ErrorResponse(`Recipe not found with id of ${req.params.id}`, 404)
-      );
-    }
-
-    res.status(200).json({
-      success: true,
-      data: recipe
-    });
-  } catch (err) {
-    next(err);
+exports.getRecipe = asyncHandler(async (req, res, next) => {
+  const recipe = await Recipe.findById(req.params.id);
+  if (!recipe) {
+    return next(
+      new ErrorResponse(`Recipe not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: recipe
+  });
+});
 
 // @desc    Create new recipe
 // @route   POST /api/v1/recipes
 // @access  Private
-exports.createRecipe = async (req, res, next) => {
-  try {
-    const recipe = await Recipe.create(req.body);
+exports.createRecipe = asyncHandler(async (req, res, next) => {
+  const recipe = await Recipe.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      data: recipe
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(201).json({
+    success: true,
+    data: recipe
+  });
+});
 
 // @desc    Update recipe
 // @route   PUT /api/v1/recipes/:id
 // @access  Private
-exports.updateRecipe = async (req, res, next) => {
-  try {
-    const recipe = await Recipe.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
+exports.updateRecipe = asyncHandler(async (req, res, next) => {
+  const recipe = await Recipe.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
 
-    if (!recipe) {
-      return next(
-        new ErrorResponse(`Recipe not found with id of ${req.params.id}`, 404)
-      );
-    }
-
-    res.status(200).json({
-      success: true,
-      data: recipe
-    });
-  } catch (err) {
-    next(err);
+  if (!recipe) {
+    return next(
+      new ErrorResponse(`Recipe not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: recipe
+  });
+});
 
 // @desc    Delete recipe
 // @route   DELETE /api/v1/recipes/:id
 // @access  Private
-exports.deleteRecipe = async (req, res, next) => {
-  try {
-    const recipe = await Recipe.findByIdAndDelete(req.params.id);
+exports.deleteRecipe = asyncHandler(async (req, res, next) => {
+  const recipe = await Recipe.findByIdAndDelete(req.params.id);
 
-    if (!recipe) {
-      return next(
-        new ErrorResponse(`Recipe not found with id of ${req.params.id}`, 404)
-      );
-    }
-
-    res.status(200).json({
-      success: true,
-      data: {}
-    });
-  } catch (err) {
-    next(err);
+  if (!recipe) {
+    return next(
+      new ErrorResponse(`Recipe not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
+});
