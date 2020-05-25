d8a4c1b72ba130ee84d71357c48c573f73a6b459
Chris Legassick <chrislegassick@Chriss-Air.broadband>
2020-05-25 16:16:07 +0100

    Updated errorHandler to check for ValidationError

--- middleware/error.js
+++ middleware/error.js
@@ -12,6 +12,11 @@ const errorHandler = (err, req, res, next) => {
     error = new ErrorResponse(message, 404);
   }
 
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map(val => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+
   res.status(error.statusCode || 500).json({
     success: false,
     error: error.message || 'Server Error'
