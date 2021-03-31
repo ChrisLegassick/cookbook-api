549d680730b18113699d270d17f91ec14525e438
Chris Legassick <chrislegassick@Chriss-Air.broadband>
2021-03-31 08:45:10 +0100

    Changed CORS to enable local host and production URL

--- server.js
+++ server.js
@@ -32,9 +32,7 @@ app.use(xssClean());
 
 app.use(hpp());
 
-app.use(
-  cors({ origin: 'https://legassick-recipe.netlify.app', credentials: true })
-);
+app.use(cors());
 
 app.use('/api/v1/recipes', recipes);
 app.use('/api/v1/auth', auth);
