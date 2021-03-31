d92ec24dfd89d9cbc0e2aafacf4a91391d4a4f23
Chris Legassick <chrislegassick@Chriss-Air.broadband>
2021-03-31 08:33:16 +0100

    Update for cookies

--- server.js
+++ server.js
@@ -32,7 +32,9 @@ app.use(xssClean());
 
 app.use(hpp());
 
-app.use(cors({ origin: true, credentials: true }));
+app.use(
+  cors({ origin: 'https://legassick-recipe.netlify.app/', credentials: true })
+);
 
 app.use('/api/v1/recipes', recipes);
 app.use('/api/v1/auth', auth);
